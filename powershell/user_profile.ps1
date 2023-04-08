719e80a6d0a36a162b09805e74c71cf900d95803
maith <maithaen122@gmail.com>
2023-04-08 10:18:01 +0700

    remove dir alias

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -1,10 +1,12 @@
 # set PowerShell to UTF-8
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
-Import-Module posh-git
 $omp_config = Join-Path $PSScriptRoot ".\agnoster.omp.json"
 oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
 
+# module
+Import-Module z
+Import-Module posh-git
 Import-Module -Name Terminal-Icons
 
 # PSReadLine
@@ -21,12 +23,14 @@ Set-PSReadLineOption -PredictionViewStyle ListView
 $env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
 
 # Alias
+Remove-Alias -Name dir
 Set-Alias -Name vim -Value nvim
 Set-Alias g git
 Set-Alias grep findstr
 Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
-Set-Alias ls 'C:\Program Files\Git\usr\bin\ls.exe'
+Set-Alias ll 'C:\Program Files\Git\usr\bin\ls.exe'
+Set-Alias lo 'C:\Windows\System32\where.exe'
 
 
 
